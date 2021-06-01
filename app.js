5f17e3b99352e7430e4015e127e0b971fbc505a0
Derek Owusu Bekoe <dowusubekoe@outlook.com>
2021-05-31 22:14:13 -0400

    Passport

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ const partnerRouter = require("./routes/partnerRouter");
 const mongoose = require("mongoose");
 const session = require("express-session");
 const FileStore = require("session-file-store")(session);
+const passport = require("passport");
+const authenticate = require("./authenticate");
 
 const url = "mongodb://localhost:27017/nucampsite";
 const connect = mongoose.connect(url, {
@@ -36,6 +38,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
@@ -46,20 +50,14 @@ app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
 function auth(req, res, next) {
-	console.log(req.session);
+	console.log(req.user);
 
-	if (!req.session.user) {
+	if (!req.user) {
 		const err = new Error("You are not authenticated!");
 		err.status = 401;
 		return next(err);
 	} else {
-		if (req.session.user === "authenticated") {
-			return next();
-		} else {
-			const err = new Error("You are not authenticated!");
-			err.status = 401;
-			return next(err);
-		}
+		return next();
 	}
 }
 
